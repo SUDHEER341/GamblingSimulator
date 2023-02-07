ac20ac4053ead595f8195c40a66d7eab34e98c4c
SUDHEER341 <sudheer601220@gmail.com>
2023-02-07 14:30:09 +0530

    Added : start with 00 every day and bet  every game

---
+++ Gamblingsimulator.java
@@ -0,0 +1,10 @@
+package com.gamblingsimulator;
+
+public class Gamblingsimulator {
+
+    public static final int INITIAL_BET_STAKE = 100;
+    public static final int BET_AMT = 1;
+    public static void main(String[] args) {
+
+    }
+}
